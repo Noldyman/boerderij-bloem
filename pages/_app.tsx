4cc32542e8525306d484766f4292c993a5e658cd
Noud Verstijnen <noud.verstijnen@gmail.com>
2023-05-25 21:43:51 +0200

    add under construction alert

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,14 @@ import "@/styles/news.scss";
 import "@/styles/contact.scss";
 import Layout from "@/components/layout/layout";
 import type { AppProps } from "next/app";
+import { Alert } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Alert severity="warning" variant="filled">
+        Deze website is onder constructie. De informatie op de website kan onjuist zijn.
+      </Alert>
       <Component {...pageProps} />
     </Layout>
   );
