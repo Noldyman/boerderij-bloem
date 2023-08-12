b41e0f2603adb7feb7fd3f380504729a93ffc5dd
Noud Verstijnen <noud.verstijnen@gmail.com>
2023-08-12 19:20:12 +0200

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,19 @@ import "@/styles/contact.scss";
 import Layout from "@/components/layout/layout";
 import type { AppProps } from "next/app";
 import { Alert } from "@mui/material";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Alert severity="error" variant="outlined">
-        Deze website is onder constructie. De informatie op de website kan onjuist zijn en het kan
-        zijn dat er dingen niet werken.
-      </Alert>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Alert severity="error" variant="outlined">
+          Deze website is onder constructie. De informatie op de website kan onjuist zijn en het kan
+          zijn dat er dingen niet werken.
+        </Alert>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+    </>
   );
 }
